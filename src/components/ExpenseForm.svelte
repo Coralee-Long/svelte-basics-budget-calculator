074b567f7a36686224e9833df21dd95a4220c625
Coralee Long <coralee.long.partner@decathlon.de>
2022-07-25 09:25:12 +0200

    js preventDefault vs. Svelte Modifier preventDefault

--- src/components/ExpenseForm.svelte
+++ src/components/ExpenseForm.svelte
@@ -1,32 +1,51 @@
 <script>
-    import Title from './SectionTitle.svelte';
-    let name ='';   // empty string: falsey
-    let amount = null;  // null: falsey
+  import Title from "./SectionTitle.svelte";
+  let name = ""; // empty string: falsey
+  let amount = null; // null: falsey
 
-    $: isEmpty = !name || !amount;
-        // If {name} is truthy, console log:
-    if (name) {
-       console.log('name value is truthy');
-    };
+  $: isEmpty = !name || !amount;
 
-  
+  // Vanilla JS way to prevent Default on form:
+
+  //   const formHandler = (e) => {
+  //     e.preventDefault();
+  //   };
+
+  const formHandler = () => {
+    console.log("form submitted");
+    console.log({ name, amount });
+    name = "";
+    amount = null;
+    console.log({ name, amount });
+  };
 </script>
 
 <section class="form">
-    <Title title='Add Expense'/>
-    <form action="" class="expense-form">
-        <div class="form-control">
-            <label for="name">Name</label>
-            <input type="text" id='name' bind:value={name}>
-        </div>
-        <div class="form-control">
-            <label for="amount">Amount</label>
-            <input type="number" id='amount' bind:value={amount}>
-        </div>
-        {#if isEmpty}
-        <p class="form-empty">Please fill out all fields</p> 
-        {/if}
-        <button disabled={isEmpty} type='submit' class='btn btn-block' class:disabled={isEmpty}>Add Expense</button> 
-        <button type='button' class='close-btn'><i class="fas fa-times"/>Close</button>
-    </form>
+  <Title title="Add Expense" />
+  <!-- // Vanilla JS way to prevent Default on form: -->
+  <!-- <form action="" class="expense-form" on:submit={formHandler}> -->
+
+  <!-- // Svelte in-line modifier method: -->
+  <form action="" class="expense-form" on:submit|preventDefault={formHandler}>
+    <div class="form-control">
+      <label for="name">Name</label>
+      <input type="text" id="name" bind:value={name} />
+    </div>
+    <div class="form-control">
+      <label for="amount">Amount</label>
+      <input type="number" id="amount" bind:value={amount} />
+    </div>
+    {#if isEmpty}
+      <p class="form-empty">Please fill out all fields</p>
+    {/if}
+    <button
+      disabled={isEmpty}
+      type="submit"
+      class="btn btn-block"
+      class:disabled={isEmpty}>Add Expense</button
+    >
+    <button type="button" class="close-btn"
+      ><i class="fas fa-times" />Close</button
+    >
+  </form>
 </section>
